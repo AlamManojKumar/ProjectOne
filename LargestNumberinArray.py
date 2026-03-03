382514c9aefcfdced220490cdba6c9b20b4862cf
Manoj <manojkumaralamd365@gmail.com>
2026-03-03 11:10:23 +0530

    Changes are added AlamManojKumars.txt

--- LargestNumberinArray.py
+++ LargestNumberinArray.py
@@ -4,5 +4,8 @@ largest = numbers[0]
 for num in numbers:
     if num > largest:
         largest = num
-print("The Largest Number" )    
+print("The Largest Number" )
+
+
+
  
---
+++ SumOfArray.py
@@ -0,0 +1,7 @@
+#Sum of Array
+total =0
+numbers = [10,20,30,40]
+for num in numbers:
+    total =total +num
+print("Sum is",total)
+
